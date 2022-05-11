d8ef34281d30f86b77ced484819e21455e773d06
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-11 14:28:24 -0300

    :art: Creating the theme.ts (colors, spacings, layers, etc)

---
+++ src/styles/theme.ts
@@ -0,0 +1,58 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family: 'Poppins, sans-serif',
+    thin: 100,
+    extralight: 200,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    }
+  },
+  colors: {
+    primary: '#363636',
+    secondary: '#34ABD0',
+    white: '#FAFAFA',
+    black: '#222222',
+    gray: '#7D7D7D',
+    light: '#F3F3F3',
+    hot: '#CC2828'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+}
