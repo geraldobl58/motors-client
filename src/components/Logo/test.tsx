9b1655347c80ed80a17f0c5a7783b966041807ca
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-11 18:28:43 -0300

    :sparkles: Creating first tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,7 +1,22 @@
 import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render a primary label by default', () => {})
+  it('should render a primary label by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/motors/i).parentElement).toHaveStyle({
+      color: '#363636'
+    })
+  })
+
+  it('should render a secondary label when color is passed', () => {
+    renderWithTheme(<Logo color="secondary" />)
+
+    expect(screen.getByLabelText(/motors/i).parentElement).toHaveStyle({
+      color: '#34ABD0'
+    })
+  })
 })
