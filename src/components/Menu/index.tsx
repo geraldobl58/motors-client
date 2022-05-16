4bb1423b89577ad1698b858ceca578c36fc31406
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-16 19:01:00 -0300

    :sparkles: Creating the mobile version of the Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { FavoriteBorder as FavoriteBorderIcon } from '@styled-icons/material/FavoriteBorder'
+
+import Logo from 'components/Logo'
+
+import * as S from './styles'
+
+const Menu = () => (
+  <S.Wrapper>
+    <S.IconWrapper>
+      <MenuIcon />
+    </S.IconWrapper>
+    <S.LogoWrapper>
+      <Logo />
+    </S.LogoWrapper>
+    <S.MenuGroup>
+      <S.IconWrapper>
+        <SearchIcon />
+      </S.IconWrapper>
+      <S.IconWrapper>
+        <FavoriteBorderIcon />
+      </S.IconWrapper>
+    </S.MenuGroup>
+  </S.Wrapper>
+)
+
+export default Menu
