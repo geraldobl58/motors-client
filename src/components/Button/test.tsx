0f6ee0de15202c10cfdb7db352b65c610d04737e
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-16 14:02:43 -0300

    :sparkles: Creating tests for button sizes

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -4,9 +4,32 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Button />)
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Ver Mais</Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Ver Mais/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Ver Mais</Button>)
+
+    expect(screen.getByRole('button', { name: /Ver Mais/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Ver Mais</Button>)
+
+    expect(screen.getByRole('button', { name: /Ver Mais/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
   })
 })
