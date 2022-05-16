03306400214bd69b8a71342250d956dfe9d7c635
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-16 13:51:53 -0300

    :sparkles: Creating the different sizes for the Button

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Button />)
+
+    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+  })
+})
