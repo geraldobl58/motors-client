cce527e7394ed15b968cb90f831668aa7da6b4e0
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-19 15:07:13 -0300

    :sparkles: Writing tests

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,12 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img:
+    'https://www.autoo.com.br/fotos/2021/11/1280_960/ranger2_26112021_68920_1280_960.jpg',
+  title: 'NOVA RANGER 2023',
+  subtitle: '<p>V6 TURBODIESEL</p>',
+  buttonLabel: 'Ver Mais',
+  buttonLink: '/vehicles/nova-ranger'
+}
+
 describe('<Banner />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Banner />)
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /nova ranger 2023/i })
+    ).toBeInTheDocument()
 
+    expect(
+      screen.getByRole('heading', { name: /turbodiesel/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /ova ranger/i })).toBeInTheDocument()
   })
 })
