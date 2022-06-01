d692d758e49a35cb1e56b80923ddaaa029caa202
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-01 18:19:10 -0300

    :sparkles: Creating Tests for Showcase

--- src/components/Showcase/test.tsx
+++ src/components/Showcase/test.tsx
@@ -1,14 +1,29 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Showcase from '.'
 
+import cardsMock from 'components/CardSlider/mock'
+
+const props = {
+  title: 'Recomendados',
+  subtitle: 'Realize seu sonho e encontre seu veículo ideal',
+  cards: cardsMock.slice(0, 1)
+}
+
 describe('<Showcase />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Showcase />)
+  it('should render full showcase', () => {
+    renderWithTheme(<Showcase {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /Showcase/i })
+      screen.getByRole('heading', { name: /Recomendados/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/Realize/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: cardsMock[0].title })
     ).toBeInTheDocument()
   })
 })
