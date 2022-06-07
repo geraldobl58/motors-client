51d6c436b9193a9bfb4b65f60c25761c7dee2d3b
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-07 18:52:47 -0300

    :sparkles: Creating the thumbnails slider

---
+++ src/components/Gallery/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: '/img/cars/1.jpg',
+    label: 'Gallery Image 1'
+  },
+  {
+    src: '/img/cars/2.jpg',
+    label: 'Gallery Image 2'
+  },
+  {
+    src: '/img/cars/3.jpg',
+    label: 'Gallery Image 3'
+  },
+  {
+    src: '/img/cars/4.jpg',
+    label: 'Gallery Image 4'
+  },
+  {
+    src: '/img/cars/5.jpg',
+    label: 'Gallery Image 5'
+  },
+  {
+    src: '/img/cars/6.jpg',
+    label: 'Gallery Image 6'
+  }
+]
---
+++ src/components/Gallery/styles.ts
@@ -0,0 +1,93 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    .slick-prev,
+    .slick-next {
+      display: block;
+      color: ${theme.colors.gray};
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      width: 5rem;
+      height: 5rem;
+      padding: 1rem;
+      border-radius: 50%;
+      z-index: ${theme.layers.overlay};
+      transform: translate(0, -50%);
+      background-color: ${theme.colors.white};
+    }
+    .slick-prev {
+      left: ${theme.spacings.large};
+    }
+    .slick-next {
+      right: ${theme.spacings.large};
+    }
+    .slick-prev.slick-disabled,
+    .slick-next.slick-disabled {
+      visibility: hidden;
+    }
+    .slick-slide > div {
+      cursor: pointer;
+    }
+    .slick-list {
+      margin: 0 -${theme.spacings.xsmall};
+    }
+    ${media.lessThan('huge')`
+      overflow-x: hidden;
+    `}
+  `}
+`
+
+export type ModalProps = {
+  isOpen: boolean
+}
+
+const modalModifiers = {
+  open: () => css`
+    opacity: 1;
+  `,
+
+  close: () => css`
+    opacity: 0;
+    pointer-events: none;
+  `
+}
+
+export const Modal = styled.div<ModalProps>`
+  ${({ theme, isOpen }) => css`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: ${theme.layers.modal};
+    transition: opacity ${theme.transition.default};
+    ${isOpen && modalModifiers.open()}
+    ${!isOpen && modalModifiers.close()}
+  `}
+`
+
+export const Close = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    position: absolute;
+    left: 0;
+    top: 0;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    text-align: right;
+  `}
+`
+
+export const Content = styled.div`
+  max-width: min(100rem, 100%);
+  max-height: 50rem;
+  margin-top: -40rem;
+`
