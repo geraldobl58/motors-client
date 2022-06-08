9393f64bca3ea4fde8593ec1b073726c3a03fae0
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-08 19:24:04 -0300

    :sparkles: Creating modal close method

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -32,9 +32,20 @@ describe('<Gallery />', () => {
     )
     expect(modal.getAttribute('aria-hidden')).toBe('false')
     expect(modal).toHaveStyle({ opacity: 1 })
+  })
 
-    // fireEvent.click(screen.getByLabelText(/close menu/i))
-    // expect(modal.getAttribute('aria-hidden')).toBe('true')
-    // expect(modal).toHaveStyle({ opacity: 0 })
+  it('should handle close modal when overlay or button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
