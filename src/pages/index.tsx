56791b3b291a649cc83a3770df6b51316bfcd58c
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-20 19:04:48 -0300

    :sparkles: Creating the Apollo Client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 
 import bannersMock from 'components/BannerSlider/mock'
 import cardMock from 'components/CardSlider/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getVehicles {
+        vehicles {
+          titulo
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
