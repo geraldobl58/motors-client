28b391797ac00d53c9a88897921b6a0c70079e93
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-21 19:54:22 -0300

    :sparkles: Using Apollo Codegen to generate query types

---
+++ src/graphql/generated/QueryVehicles.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { ENUM_VEHICLE_COMBUSTIVEL, ENUM_VEHICLE_CAMBIO } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: QueryVehicles
+// ====================================================
+
+export interface QueryVehicles_vehicles_make {
+  __typename: "Categorias";
+  nome: string;
+}
+
+export interface QueryVehicles_vehicles_cover {
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryVehicles_vehicles_version {
+  __typename: "Versao";
+  nome: string;
+}
+
+export interface QueryVehicles_vehicles_localization {
+  __typename: "Localization";
+  nome: string;
+}
+
+export interface QueryVehicles_vehicles {
+  __typename: "Vehicle";
+  make: QueryVehicles_vehicles_make | null;
+  titulo: string;
+  slug: string;
+  cover: QueryVehicles_vehicles_cover | null;
+  version: QueryVehicles_vehicles_version | null;
+  combustivel: ENUM_VEHICLE_COMBUSTIVEL;
+  cambio: ENUM_VEHICLE_CAMBIO | null;
+  preco: number;
+  ano: string;
+  kilometragem: number | null;
+  localization: QueryVehicles_vehicles_localization | null;
+}
+
+export interface QueryVehicles {
+  vehicles: (QueryVehicles_vehicles | null)[] | null;
+}
+
+export interface QueryVehiclesVariables {
+  limit: number;
+}
---
+++ src/graphql/generated/globalTypes.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum ENUM_VEHICLE_CAMBIO {
+  Automatico = "Automatico",
+  Automatizado = "Automatizado",
+  Manual = "Manual",
+}
+
+export enum ENUM_VEHICLE_COMBUSTIVEL {
+  Alcool = "Alcool",
+  Flex = "Flex",
+  Gasolina = "Gasolina",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
