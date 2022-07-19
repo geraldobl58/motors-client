1861f6bdffe2871e75659ee4e28ec8795b8f2295
Geraldo Luiz <geraldobl58@gmail.com>
2022-07-19 15:15:12 -0300

    :sparkles: Adding icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,8 +18,8 @@ function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <Head>
           <title>Motors</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="shortcut icon" href="/img/icon-192.png" />
+          <link rel="apple-touch-icon" href="/img/icon-192.png" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="description" content="" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
