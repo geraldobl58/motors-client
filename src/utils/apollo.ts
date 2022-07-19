ec779b2a7e098c1059d97408478491ceeba619dd
Geraldo Luiz <geraldobl58@gmail.com>
2022-07-19 10:34:18 -0300

    :sparkles: Creating environment variables

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -14,7 +14,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject | null>
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    link: new HttpLink({ uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql` }),
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
